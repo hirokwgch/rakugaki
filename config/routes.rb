aa447ff1614ff98bed18cf1ba779bd377ebb09b3
Hiroki Kawaguchi <hiroki.kwgch@gmail.com>
2017-08-17 23:30:06 +0900

    ルートパスでアクセス可能にした

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'canvas/new'
-  get 'canvas/release'
+  namespace :canvas do
+    get 'new'
+    get 'release'
+  end
+
+  get '/', to: 'canvas#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
